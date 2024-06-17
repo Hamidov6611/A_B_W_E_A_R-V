c86d26b32d9ff80b4d5082d193009a431c56e381
Dilmurod <dilmuroddelitot@gmail.com>
2024-06-17 23:54:40 +0500

    Add Cookie parser middleware

--- helpers/connect.js
+++ helpers/connect.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
 
 const bootstrap = async (app) => {
+    const PORT = process.env.PORT
     try {
         await mongoose.connect(process.env.DB_URL).then(() => console.log("Connected DB"))
-        app.listen(process.env.PORT, () => console.log(`Server started on port http://localhost:${process.env.PORT}`))
+        app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`))
     } catch (error) {
         console.log(error)
     }
