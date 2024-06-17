d9a4d06506d52e438fbd49e11d37a9948d8823ec
Dilmurod <dilmuroddelitot@gmail.com>
2024-06-17 23:53:34 +0500

    Auth service

--- service/auth.service.js
+++ service/auth.service.js
@@ -1,6 +1,8 @@
 const UserDto = require("../dtos/user.dto");
 const userModel = require("../models/user.model")
 const { hash, verify } = require('argon2');
+const tokenSevice = require("./token.sevice");
+const mailService = require("./mail.service");
 
 class AuthService {
 
@@ -15,10 +17,15 @@ class AuthService {
         const hashedPassword = await hash(password)
 
         const user = await userModel.create({ email, password: hashedPassword })
-
         const userDto = new UserDto(user)
 
-        return { data: userDto }
+        await mailService.sendMail(email, `${process.env.API_URL}/api/auth/activation/${userDto.id}`)
+
+
+        const tokens = tokenSevice.generateTokens({ ...userDto })
+
+        await tokenSevice.saveToken(userDto.id, tokens.refreshToken)
+        return { user: userDto, ...tokens }
     }
 
     async activate(id) {
@@ -30,6 +37,56 @@ class AuthService {
 
         user.isActivated = true
         await user.save()
+    }
+
+    async login(email, password) {
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            throw new Error("User not found")
+        }
+ 
+        const isPassEquals = await verify(user.password, password)
+
+        if (!isPassEquals) {
+            throw new Error("Password is wrong")
+        }
+
+        const userDto = new UserDto(user)
+
+        const tokens = tokenSevice.generateTokens({ ...userDto })
+
+        await tokenSevice.saveToken(userDto.id, tokens.refreshToken)
+
+        return { user: userDto, ...tokens }
+    }
+
+    async logout(refreshToken) {
+        return await tokenSevice.removeToken(refreshToken)
+    }
+
+    async refresh(refreshToken) {
+        if(!refreshToken) {
+            throw new Error("No refresh token")
+        }
+
+        const userPayload = tokenSevice.validateRefreshToken(refreshToken)
+
+        const tokenFromDb = await tokenSevice.findToken(refreshToken)
+
+        if (!userPayload || !tokenFromDb) {
+            throw new Error("No user")
+        }
+
+        const user = await userModel.findById(userPayload.id)
+
+        const userDto = new UserDto(user)
+
+        const tokens = tokenSevice.generateTokens({ ...userDto })
+
+        await tokenSevice.saveToken(userDto.id, tokens.refreshToken)
+
+        return { user: userDto, ...tokens }
     }
 
 }
