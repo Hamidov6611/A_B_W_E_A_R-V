2e313e1a38637eabf996f796bd46a4021a11cf20
Dilmurod <dilmuroddelitot@gmail.com>
2024-06-17 11:07:04 +0500

    Remove all specified files and folders

--- models/user.model.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require("mongoose")
-
-const UserSchema = new Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    isActivated: {type: Boolean, default: false}
-}, {timestamps: true})
-
-module.exports = model("User", UserSchema) 
--- routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const authController = require("../controllers/auth.controller.js")
-
-const authRoute = express.Router()
-
-authRoute.post("/register", authController.register)
-authRoute.get("/activation/:id", authController.activate)
-
-module.exports = authRoute
