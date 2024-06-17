9accdde45c889897b2a6f7606598885b916b8ca6
Dilmurod <dilmuroddelitot@gmail.com>
2024-06-17 23:54:05 +0500

    Auth route

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,5 +5,8 @@ const authRoute = express.Router()
 
 authRoute.post("/register", authController.register)
 authRoute.get("/activation/:id", authController.activate)
+authRoute.post("/login", authController.login)
+authRoute.post("/logout", authController.logout)
+authRoute.get("/refresh", authController.refresh)
 
 module.exports = authRoute
