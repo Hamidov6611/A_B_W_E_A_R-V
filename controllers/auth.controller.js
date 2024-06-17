80ca8d99097d8a97dcb401117fa33e7dfbf242a7
Dilmurod <dilmuroddelitot@gmail.com>
2024-06-17 23:53:03 +0500

    Auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,8 +4,9 @@ class AuthController {
     async register(req, res, next) {
         try {
             const { email, password } = req.body
-
             const data = await authService.register(email, password)
+
+            res.cookie("refreshToken", data.refreshToken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 })
 
             res.status(201).json(data)
         } catch (error) {
@@ -21,9 +22,44 @@ class AuthController {
 
             await authService.activate(userId)
 
-            return res.json({ message: "Account activated" })
+            return res.redirect(process.env.CLIENT_URL)
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    async login(req, res, next) {
+        try {
+            const { email, password } = req.body
+            const data = await authService.login(email, password)
+
+            res.cookie("refreshToken", data.refreshToken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 })
+
+            res.status(200).json(data)
+        } catch (error) {
+            next(error.message)
+        }
+    }
+
+    async logout(req, res, next) {
+        try {
+            const { refreshToken } = req.cookies
+            const token = await authService.logout(refreshToken)
+            res.clearCookie("refreshToken")
+            res.status(200).json(token)
+        } catch (error) {
+            next(error.message)
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+            const { refreshToken } = req.cookies
+            const data = await authService.refresh(refreshToken)
+            res.cookie("refreshToken", data.refreshToken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 })
+            res.status(200).json(data)
+        } catch (error) {
+            next(error.message)
         }
     }
 }
